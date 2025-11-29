0cb3818f9062a0aaff7f2c772ef92fa274a2d0dd
joy-mahmud <joymahmudiucse@gmail.com>
2025-11-29 23:34:25 +0600

    payment create api

--- educare/urls.py
+++ educare/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/students/", include('students.urls')),
+    path("api/payment/", include('payment.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Student
 # Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'studentName', 'mobile')
+    search_fields = ('studentName', 'mobile')
+    list_filter = ('studentName',)
+    ordering = ('id',)
